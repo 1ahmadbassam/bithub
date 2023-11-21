467422b267c8eb6b1b3ab46d14b7cbce4407f427
Ahmad Bassam El Bizri <34858431+1ahmadbassam@users.noreply.github.com>
2023-11-21 14:20:07 +0200

    Format

--- frontend/login_module.py
+++ frontend/login_module.py
@@ -1,15 +1,17 @@
-#before you run this code, you have to  run the command "pip install customtkinter" on your terminal
+# before you run this code, you have to  run the command "pip install customtkinter" on your terminal
 
 import customtkinter
 import hashlib
 
-#need serialization and deserialization
+# need serialization and deserialization
 accounts = {}
+
 
 def hash_password(password):
     hash_object = hashlib.sha256()
     hash_object.update(password.encode())
     return hash_object.hexdigest()
+
 
 def create_new_account(username, password):
     if username in accounts:
