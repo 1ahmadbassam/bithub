e71df78b387ed1a602a393c5c45fee5f1d6cd151
RubaHoussami <houssamiruba@gmail.com>
2023-11-25 21:00:31 +0200

    added security features

--- security.py
+++ security.py
@@ -1,4 +1,10 @@
+import hashlib
+import pickle
+import os
+
 BLOCKING_HTML = "blocked.html"
+SECURITY_DIRECTIVE = "security"
+users = set()
 
 BLOCKED_IP_ADDRESSES = {"172.20.10.3"}
 BLOCKED_HOSTNAMES = {"steptail.com", "www.google.com", "www.microsoft.com"}
@@ -9,3 +15,37 @@ SECURED_WEBSITES = {"frogfind.com"}
 def load_blocking_html():
     with open(BLOCKING_HTML, "rb") as file:
         return file.read()
+
+
+def hash_credentials(credentials: str) -> str:
+    hash_object = hashlib.sha256()
+    hash_object.update(credentials.encode())
+    return hash_object.hexdigest()
+
+
+def add_hashed_credentials_to_users(credentials: str):
+    users.add(hash_credentials(credentials))
+
+
+def add_new_user(credentials: str):
+    users.add(hash_credentials(credentials))
+
+
+def authenticate(credentials: str):
+    if hash(credentials) in users:
+        return True
+    return False
+
+
+def save_hashed_credentials(credentials: str):
+        os.makedirs(SECURITY_DIRECTIVE, exist_ok=True)
+        with open(SECURITY_DIRECTIVE, "wb") as file:
+            file.write(pickle.dumps(users))
+        print("[INFO] Saved credentials for the website.")
+
+
+def load_hashed_credentials():
+    global users
+    if os.path.exists(SECURITY_DIRECTIVE):
+        with open(SECURITY_DIRECTIVE, "rb") as file:
+            users = pickle.loads(file.read())
