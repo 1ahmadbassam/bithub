1c1780b03ae84f4330c53477b41e81f5be9bdf30
RubaHoussami <houssamiruba@gmail.com>
2023-11-26 19:30:24 +0200

    added user interface features

--- frontend/login_module.py
+++ frontend/login_module.py
@@ -3,13 +3,32 @@
 import customtkinter as ctk
 import hashlib
 from httplib import requests
-from security import load_hashed_credentials, save_hashed_credentials, hash_credentials
+from security import hash_credentials
+import os
+import pickle
 
 ctk.set_appearance_mode("Light")
+
+ADMINISTRATOR_DIRECTIVE = "administrator"
+ADMINISTRARTOR_FILE = ADMINISTRATOR_DIRECTIVE + "users.dat"
 
 users = {}
 
 req = '''GET http://sdfox7.com/ HTTP/1.1\r\nAccept: image/gif, image/x-xbitmap, image/jpeg, image/pjpeg, */*\r\nAccept-Language: en\r\nUA-pixels: 1024x768\r\nUA-color: color16\r\nUA-OS: Windows NT\r\nUA-CPU: x86\r\nUser-Agent: Mozilla/2.0 (compatible; MSIE 3.01; Windows NT)\r\nHost: sdfox7.com\r\nConnection: Keep-Alive\r\n\r\nGET http://sdfox7.com/xp/authcab.jpg HTTP/1.1\r\nHost: sdfox7.com\r\nConnection: keep-alive\r\n\r\nhttp://sdfox7.com/xp/ie8cert.jpg'''
+
+
+def save_hashed_credentials():
+    os.makedirs(ADMINISTRATOR_DIRECTIVE, exist_ok=True)
+    with open(ADMINISTRARTOR_FILE, "wb") as file:
+        file.write(pickle.dumps(users))
+    print("[INFO] Saved credentials for the website.")
+
+
+def load_hashed_credentials():
+    global users
+    if os.path.exists(ADMINISTRARTOR_FILE):
+        with open(ADMINISTRARTOR_FILE, "rb") as file:
+            users = pickle.loads(file.read())
 
 
 def create_new_account(username: str, password: str) -> bool:
@@ -27,9 +46,17 @@ def create_new_account(username: str, password: str) -> bool:
         return True
 
 
+def Print(printable: str):
+    tabview = ctk.CTkTabview(master=frame)
+    tabview.pack(padx=20, pady=0)
+    label2 = ctk.CTkLabel(master=tabview.tab("tab 1"), text=str(requests.parse(printable)), font=("Roboto", 10), cursor="hand2",
+                          text_color=("#DB3E39", "#821D1A"))
+    label2.pack(pady=1, padx=10)
+
+
 def authenticate(username: str, password: str) -> None:
     if not username:
-         open_proxy()
+        open_proxy()
     elif username not in users:
         print("username doesn't exist.")
     else:
@@ -47,11 +74,12 @@ def goBack():
 def open_proxy():
     global root
     root.iconify()  # Close the old window
-    new_window = ctk.CTkToplevel()  # Create a new window 
+    new_window = ctk.CTkToplevel()  # Create a new window
     new_window.geometry("800x500")
     new_window.title("proxy")
 
-    frame = ctk.CTkFrame(master=new_window)  # Create a frame inside the new window
+    # Create a frame inside the new window
+    frame = ctk.CTkFrame(master=new_window)
     frame.pack(pady=20, padx=60, fill="both", expand=True)
     label = ctk.CTkLabel(master=frame, text="Proxy Sever")
     label.pack(pady=15, padx=10)
@@ -72,34 +100,40 @@ def open_proxy():
 
 
 def open_registration_window():
-    global registration_window, root
+    global registration_window, root, users
+    load_hashed_credentials()
     root.iconify()  # Close the old window
-    registration_window = ctk.CTkToplevel()  # Create a new window 
+    registration_window = ctk.CTkToplevel()  # Create a new window
     registration_window.title("proxy")
     registration_window.geometry("800x500")
     registration_window.title("Registration")
 
-    frame = ctk.CTkFrame(master=registration_window)  # Create a frame inside the new window
+    # Create a frame inside the new window
+    frame = ctk.CTkFrame(master=registration_window)
     frame.pack(pady=100, padx=60, fill="both", expand=True)
 
-    label = ctk.CTkLabel(master=frame, text="Create a new account", font=("Roboto", 20))
+    label = ctk.CTkLabel(
+        master=frame, text="Create a new account", font=("Roboto", 20))
     label.pack(pady=25, padx=10)
 
     username = ctk.CTkEntry(master=frame, placeholder_text="Username")
     username.pack(pady=5, padx=12)
 
-    password = ctk.CTkEntry(master=frame, placeholder_text="Password", show="*")
+    password = ctk.CTkEntry(
+        master=frame, placeholder_text="Password", show="*")
     password.pack(pady=12, padx=12)
 
     button = ctk.CTkButton(master=frame, text="Create account",
-                           command=lambda: create_new_account(username.get(), password.get()),
+                           command=lambda: create_new_account(
+                               username.get(), password.get()),
                            fg_color=("#DB3E39", "#821D1A"), hover_color=("#DB3E39", "#821D1A"), hover=True)
     button.pack(pady=12, padx=12)
 
     label2 = ctk.CTkLabel(master=frame, text="Already have an account, sign in here", font=("Roboto", 10),
                           cursor="hand2", text_color=("#DB3E39", "#821D1A"))
     label2.pack(pady=1, padx=10)
-    label2.bind("<Button-1>", lambda event: goBack())  # Bind the click event to go back to the sign-in page
+    # Bind the click event to go back to the sign-in page
+    label2.bind("<Button-1>", lambda event: goBack())
 
 
 def open_sign_in():
@@ -113,15 +147,16 @@ def open_sign_in():
 
     frame = ctk.CTkFrame(master=root)
     frame.pack(pady=100, padx=60, fill="both", expand=True)
-    
 
-    label = ctk.CTkLabel(master=frame, text="Login System", font=("Roboto", 24))
+    label = ctk.CTkLabel(
+        master=frame, text="Login System", font=("Roboto", 24))
     label.pack(pady=30, padx=10)
 
     username = ctk.CTkEntry(master=frame, placeholder_text="Username")
     username.pack(pady=0, padx=12)
 
-    password = ctk.CTkEntry(master=frame, placeholder_text="Password", show="*")
+    password = ctk.CTkEntry(
+        master=frame, placeholder_text="Password", show="*")
     password.pack(pady=12, padx=12)
 
     button = ctk.CTkButton(master=frame, text="Login", command=lambda: authenticate(username.get(), password.get()),
@@ -138,6 +173,3 @@ def open_sign_in():
                 lambda event: open_registration_window())  # Bind the click event to open the registration window
 
     root.mainloop()
-
-
-
