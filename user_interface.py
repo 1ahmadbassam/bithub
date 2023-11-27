329d650689f1e70e62ecf9aaa6cd7b6deda44aad
hawraakhalil <hawraakhalil.05@gmail.com>
2023-11-27 13:00:32 +0200

    commnted the frontend

--- user_interface.py
+++ user_interface.py
@@ -16,7 +16,7 @@ ADMINISTRATOR_FILE = ADMINISTRATOR_DIRECTIVE + "users.dat"
 
 users = {}
 
-
+# create a class that redirects the console output to the text area frame
 class ConsoleRedirector:
     def __init__(self, text_area):
         self.text_area = text_area
@@ -45,14 +45,17 @@ def load_hashed_credentials():
         with open(ADMINISTRATOR_FILE, "rb") as file:
             users = pickle.loads(file.read())
 
-
+# a function that creates a new account
 def create_new_account(username: str, password: str) -> bool:
     global registration_window
+    # check if username and password fields are empty
     if not username or not password:
         print("[Warning] Empty username or password.")
         return False
+    # check if username already exists
     elif username in users:
         print("[Warning] Username already exists, try a different username.")
+    # save username and password to users
     else:
         users[username] = hash_credentials(password)
         print(users)
@@ -61,7 +64,7 @@ def create_new_account(username: str, password: str) -> bool:
         return True
 
 
-# function that checks that the username and password exist and match in the users dictionary
+# a function that checks that the username and password exist and match in the users dictionary
 def authenticate(username: str, password: str) -> None:
     global proxy_window
     if username not in users:
@@ -69,7 +72,7 @@ def authenticate(username: str, password: str) -> None:
     elif users[username] == hash_credentials(password):
         sign_in_window.destroy()
         proxy_window.deiconify()
-        #start the server once authentication is succesful
+        # start the server once authentication is succesfull
         run_server_thread = threading.Thread(target=run_server)
         run_server_thread.daemon = True
         run_server_thread.start()
@@ -77,42 +80,42 @@ def authenticate(username: str, password: str) -> None:
         print("[Warning] Password doesn't match username.")
 
 
-# function that closes the registration window and opens the sign in window
+# a function that closes the registration window and opens the sign in window
 def go_back_to_sign_in():
     registration_window.destroy()
     open_sign_in()
 
 
-# function that closes the proxy and saves the global variables
+# a function that closes the proxy and saves the global variables
 def exit_script():
     print("[INFO] Server is terminating...")
     caching.save_globals()
     save_hashed_credentials()
     exit(0)
 
-
+# open the proxy window
 def open_proxy():
     global sign_in_window, proxy_window
 
-    # Creating the proxy window
+    # create the proxy window
     proxy_window = ctk.CTk()  
     proxy_window.geometry("800x500")
     proxy_window.title("proxy")
     proxy_window.iconify()
     open_sign_in() 
 
-    # Creating a frame inside the new window
+    # create a frame inside the new window
     frame = ctk.CTkFrame(master=proxy_window, fg_color="#821D1A")
     frame.pack(pady=20, padx=60, fill="both", expand=True)
 
     title = ctk.CTkLabel(master=frame, text="BITHUB")
     title.pack(pady=15, padx=10)
 
-    # Creating a text area widget to display console output
+    # create a text area widget to display console output
     terminal_frame = ctk.CTkTextbox(master=frame)
     terminal_frame.pack(padx=10, pady=10, fill="both", expand=True)
 
-    # Redirect console output to the text area
+    # redirect console output to the text area
     sys.stdout = ConsoleRedirector(terminal_frame)
 
     # exit button that closes the server
@@ -124,35 +127,37 @@ def open_proxy():
 def open_registration_window():
     global registration_window, sign_in_window, users
     
-    sign_in_window.destroy()  # Close the old window
-    # Creating the sign up window
+    # close the old window
+    sign_in_window.destroy()  
+
+    # create the sign up window
     registration_window = ctk.CTkToplevel()  
     registration_window.title("proxy")
     registration_window.geometry("800x500")
     registration_window.title("Registration")
 
-    # Create a frame inside the new window
+    # create a frame inside the new window
     frame = ctk.CTkFrame(master=registration_window)
     frame.pack(pady=100, padx=60, fill="both", expand=True)
 
     label = ctk.CTkLabel(master=frame, text="Create a new account", font=("Roboto", 20))
     label.pack(pady=25, padx=10)
 
-    # fields for username and password
+    # create fields for username and password
     username_for_registration = ctk.CTkEntry(master=frame, placeholder_text="Username")
     username_for_registration.pack(pady=5, padx=12)
 
     password_for_registration = ctk.CTkEntry(master=frame, placeholder_text="Password", show="*")
     password_for_registration.pack(pady=12, padx=12)
 
-    # button that creates an account and redirects you to the sign in page
+    # create a button that creates an account and redirects you to the sign in page
     create_account_button = ctk.CTkButton(master=frame, text="Create account", command=lambda: create_new_account(username_for_registration.get(), password_for_registration.get()), fg_color=("#DB3E39", "#821D1A"), hover_color=("#DB3E39", "#821D1A"), hover=True)
     create_account_button.pack(pady=12, padx=12)
 
     label2 = ctk.CTkLabel(master=frame, text="Already have an account, sign in here", font=("Roboto", 10), cursor="hand2", text_color=("#DB3E39", "#821D1A"))
     label2.pack(pady=1, padx=10)
 
-    # Bind the click event to go back to the sign-in page
+    # bind the click event to go back to the sign-in page
     label2.bind("<Button-1>", lambda event: go_back_to_sign_in())
 
 
@@ -161,7 +166,7 @@ def open_sign_in():
 
     load_hashed_credentials()
 
-    # Create the sign in window
+    # create the sign in window
     sign_in_window = ctk.CTkToplevel()
     sign_in_window.geometry("800x500")
     sign_in_window.title("login")
@@ -175,14 +180,14 @@ def open_sign_in():
     label = ctk.CTkLabel(master=frame, text="Login System", font=("Roboto", 24))
     label.pack(pady=30, padx=10)
 
-    # Username and password fields
+    # create username and password fields
     username_for_sign_in = ctk.CTkEntry(master=frame, placeholder_text="Username")
     username_for_sign_in.pack(pady=0, padx=12)
 
     password_for_sign_in = ctk.CTkEntry(master=frame, placeholder_text="Password", show="*")
     password_for_sign_in.pack(pady=12, padx=12)
 
-    # Sign in button that authenticates user then starts the proxy server
+    # create a sign-in button that authenticates user then starts the proxy server
     sign_in_button = ctk.CTkButton(master=frame, text="Login", command=lambda: authenticate(username_for_sign_in.get(), password_for_sign_in.get()), fg_color=("#DB3E39", "#821D1A"), hover_color=("#DB3E39", "#821D1A"), hover=True)
     sign_in_button.pack(pady=12, padx=12)
 
@@ -192,7 +197,7 @@ def open_sign_in():
     label2 = ctk.CTkLabel(master=frame, text="Don't have an account, create a new one here", font=("Roboto", 10), cursor="hand2", text_color=("#DB3E39", "#821D1A"))
     label2.pack(pady=1, padx=10)
 
-    # Bind the click event to open the registration window
+    # bind the click event to open the registration window
     label2.bind("<Button-1>", lambda event: open_registration_window())
 
 
